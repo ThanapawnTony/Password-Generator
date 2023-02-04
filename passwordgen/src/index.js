071434db56c1a62c8e71c2d12f0fef37d97af9c6
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2023-02-04 20:50:43 +0700

    copy icon function

--- passwordgen/src/index.js
+++ passwordgen/src/index.js
@@ -1,7 +1,15 @@
 const lengthSlider = document.querySelector(".password-length input");
+copyIcon = document.querySelector(".input-box span")
 const updateSlider = () => {
     // pass slider value to span text
     document.querySelector(".password-length span").innerText = lengthSlider.value;
 }
 updateSlider();
+
+const copyPassword = () => {
+    navigator.clipboard.writeText(value);
+    copyIcon.innerText = "check";
+}
+
+copyIcon.addEventListener("click", copyPassword);
 lengthSlider.addEventListener("input", updateSlider);
